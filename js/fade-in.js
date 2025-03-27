98a2e726389f581adb4847ccecd668f2ed1784c8
goelupi <goelupi@gmail.com>
2025-03-27 15:22:43 +0900

    js変更

--- js/fade-in.js
+++ js/fade-in.js
@@ -2,7 +2,7 @@
 
 const observer = new IntersectionObserver((entries) => { 
   entries.forEach((entry) => { 
-  if (entry.intersectionRatio > 0.3) { // 30%表示されたら 
+  if (entry.intersectionRatio > 0.1) { // 10%表示されたら 
   entry.target.classList.add('fade-in'); 
   observer.unobserve(entry.target); // 一度発火したら監視解除 
   }
