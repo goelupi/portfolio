acd064119160e311df3c24be4957907f28e3b5fb
goelupi <goelupi@gmail.com>
2025-03-27 21:57:08 +0900

    ハンバーガメニュ自動閉じる

--- js/toggle-menu-button.js
+++ js/toggle-menu-button.js
@@ -1,7 +1,7 @@
-//ハンバーガーメニューjs class名toggle-menu-button,header-site-menu
 window.addEventListener('load', function () {
   var $button = document.querySelector('.toggle-menu-button');
   var $menu = document.querySelector('.header-site-menu');
+  var $links = document.querySelectorAll('.header-site-menu a'); // メニュー内のリンクを取得
 
   $button.addEventListener('click', function () {
       // メニューの表示・非表示を切り替える
@@ -9,5 +9,14 @@ window.addEventListener('load', function () {
 
       // 三本線をバッテンにする
       $button.classList.toggle('is-active');
+  });
+
+  // メニュー内のリンクがクリックされたとき
+  $links.forEach(function ($link) {
+      $link.addEventListener('click', function () {
+          // メニューを閉じる
+          $menu.classList.remove('is-show');
+          $button.classList.remove('is-active');
+      });
   });
 });
